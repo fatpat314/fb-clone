ee18a38b3cdf9f6f5c4cce7253ea837e1840a9dd
Patrick <w.patrick.kelly@gmail.com>
2021-12-02 11:48:02 -0800

    story reel implemented

---
+++ src/Feed.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Feed.css';
+import StoryReel from './StoryReel.js'
+
+function Feed() {
+    return (
+        <div className='feed'>
+            <StoryReel />
+            {/* MessageSender */}
+
+        </div>
+    )
+}
+
+export default Feed
---
+++ src/StoryReel.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import "./StoryReel.css";
+import Story from './Story.js'
+
+function StoryReel() {
+    return (
+        <div className="storyReel">
+            <Story 
+                image="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg"
+                profileSrc="https://avatars2.githubusercontent.com/u/24712956?s=400&u=b71527e605ae1b748fc2d4157a842e57e427ad44&v=4"
+                title="Sonny Sangha"
+            />
+            <Story 
+                image="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg"
+                profileSrc="https://avatars2.githubusercontent.com/u/24712956?s=400&u=b71527e605ae1b748fc2d4157a842e57e427ad44&v=4"
+                title="Sonny Sangha"
+            />
+            <Story 
+                image="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg"
+                profileSrc="https://avatars2.githubusercontent.com/u/24712956?s=400&u=b71527e605ae1b748fc2d4157a842e57e427ad44&v=4"
+                title="Sonny Sangha"
+            />
+            <Story 
+                image="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg"
+                profileSrc="https://avatars2.githubusercontent.com/u/24712956?s=400&u=b71527e605ae1b748fc2d4157a842e57e427ad44&v=4"
+                title="Sonny Sangha"
+            />
+            <Story 
+                image="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg"
+                profileSrc="https://avatars2.githubusercontent.com/u/24712956?s=400&u=b71527e605ae1b748fc2d4157a842e57e427ad44&v=4"
+                title="Sonny Sangha"
+            />
+        </div>
+    )
+}
+
+export default StoryReel;
